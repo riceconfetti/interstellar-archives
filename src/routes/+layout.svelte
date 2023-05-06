2e0ce5bea958cf9414498cb842efa5cee6a508d2
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2023-05-06 11:30:22 -0400

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Nav from '../lib/Nav.svelte';
+</script>
+
+<Nav />
+
+<div id="overlay">
+	<slot />
+</div>
+
+<style lang="scss">
+	:global(body) {
+		font-family: Audiowide, Open Sans, Sans Serif;
+		background: url('/stars.jpg');
+	}
+
+	:global(#overlay) {
+		position: fixed; /* Sit on top of the page content */
+		width: 100%; /* Full width (cover the whole page) */
+		height: 100%; /* Full height (cover the whole page) */
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: rgba(0, 0, 0, 0.1);
+		z-index: -99; /* Specify a stack order in case you're using a different order for other elements */
+	}
+</style>
